241b3ca84d44c3da612090319c1cc81c7026f346
Anton <antonkonev2003@gmail.com>
2021-09-08 19:21:53 +0300

    Add labs 1-2

---
+++ PD-21_Konev_Anton_Labs/Lab1_var_9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_var_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v, t, s, a;
+            Console.Write("a= ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("t= ");
+            t = Convert.ToDouble(Console.ReadLine());
+            v = a / t;
+            s = v * t + (a * Math.Pow(t, 2) / 2);
+            Console.WriteLine("s= " + s);
+        }
+    }
+}
---
+++ PD-21_Konev_Anton_Labs/Lab2_var_9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_var_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, k, result;
+            result = 1;
+            Console.Write("n= ");
+            n = Convert.ToDouble(Console.ReadLine());
+            k = n * n;
+            for (double i = k; i < n * k; i++)
+            {
+                result = result * (Math.Pow(i, 2) - (Math.Pow(-1, Math.Pow(i, 2) + 1)) * i) / (Math.Pow(i, 2) + 2);
+            }
+            Console.WriteLine("result= " + result);
+        }
+    }
+}
